0e98be4d029e2944d71c55477383044547047de7
Ashu11219 <ashish.mishra12012007@gmail.com>
2026-09-20 00:06:37 +0530

    Model trained and tested

---
+++ main.py
@@ -0,0 +1,15 @@
+from evaluate import evaluate_model
+from run_demo import run_on_video
+
+def main(path):
+    print("Evaluating thermal model...")
+    evaluate_model('runs/detect/thermal_full/weights/best.pt', 'configs/thermal.yaml', 'test')
+
+    print("\nEvaluating rgb model...")
+    evaluate_model('runs/detect/rgb_full/weights/best.pt', 'configs/rgb.yaml', 'test')
+
+    print("\nRunning demo...")
+    run_on_video(path)
+
+if __name__ == "__main__":
+    main('data/videos/sample.mp4')
--- train.py
+++ train.py
@@ -1,4 +1,5 @@
 from ultralytics import YOLO
 
-model = YOLO('yolov8n.pt')
-model.train(data='configs/thermal.yaml', epochs=5, device=0, imgsz=640, batch=16)
+if __name__ == '__main__':
+    model = YOLO('yolov8n.pt')
+    model.train(data='configs/rgb.yaml', epochs=100, patience=20, device=0, imgsz=640, batch=16, name='rgb_full')
